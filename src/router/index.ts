54986ba72e97813ed5bf469ccdbfc8b95dbda02f
CentOS_API <19805318060@139.com>
2024-12-07 18:51:04 +0800

    添加路由守卫来实现页面加载进度条功能，新增NProgress工具并配置相关参数。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
+import NProgress from '@/utils/nprogress'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: setupLayouts([
     ...routes,
   ]),
+})
+
+// 路由守卫
+router.beforeEach((_to, _from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 // 需手动处理热更新
